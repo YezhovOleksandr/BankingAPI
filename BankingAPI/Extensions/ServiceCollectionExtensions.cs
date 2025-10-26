73dfb9a1246344b25fa0606fe25c01e3205dfc2f
Oleksandr Yezhov <o.yezhov.lionwood@gmail.com>
2025-10-26 14:49:26 +0200

    Clean up

---
+++ BankingAPI/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,105 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using BankingAPi.Infrastructure;
+using BankingAPi.Infrastructure.Options;
+using BankingAPI.Interfaces.Api;
+using BankingAPI.Interfaces.Domain;
+using BankingAPI.Options;
+using BankingAPI.Services.Api;
+using BankingAPI.Services.Background;
+using BankingAPI.Services.Domain;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+
+namespace BankingAPI.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddOptions(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.Configure<DatabaseOptions>(configuration.GetSection(nameof(DatabaseOptions)));
+        services.Configure<IdentityOptions>(configuration.GetSection(nameof(IdentityOptions)));
+        
+        return services;
+    }
+
+    public static IServiceCollection AddSqlDatabase(this IServiceCollection services)
+    {
+        services.AddDbContext<ApplicationDbContext>
+        (
+            (serviceProvider, opt) =>
+            {
+                var dbOptions = serviceProvider.GetRequiredService<IOptions<DatabaseOptions>>().Value;
+                opt.UseSqlServer(dbOptions.ConnectionString);
+                opt.EnableDetailedErrors();
+            }
+        );
+
+        return services;
+    }
+
+    public static IServiceCollection AddJwtAuthentication(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddAuthentication(opt =>
+        {
+            opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        }).AddJwtBearer(opt =>
+        {
+            JwtSecurityTokenHandler.DefaultMapInboundClaims = false;
+            JwtSecurityTokenHandler.DefaultOutboundClaimTypeMap.Clear();
+            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
+            var identityOptions = new IdentityOptions();
+            configuration.GetSection(nameof(IdentityOptions)).Bind(identityOptions);
+            opt.TokenValidationParameters = new TokenValidationParameters()
+            {
+                ValidateIssuer = true,
+                ValidIssuer = identityOptions.Issuer,
+                ValidAudience = identityOptions.Audience,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+
+                IssuerSigningKeyResolver = (token, securityToken, kid, parameters) =>
+                {
+                    var httpClient = new HttpClient();
+
+                    var options = configuration.GetSection(nameof(IdentityOptions)).Get<IdentityOptions>();
+            
+                    var jwks = httpClient.GetStringAsync($"{options.Issuer}/.well-known/jwks.json").Result;
+                    // Parse the fetched JWKS into a JsonWebKeySet object
+                    var keys = new JsonWebKeySet(jwks);
+
+                    return keys.Keys;
+                }
+            };
+        });
+
+        return services;
+    }
+
+    public static IServiceCollection AddServices(this IServiceCollection services)
+    {
+        services.AddScoped<IAccountService, AccountService>();
+        services.AddScoped<IUserService, UserService>();
+        services.AddScoped<IWalletService, WalletService>();
+
+        return services;
+    }
+    
+    public static IServiceCollection AddApiServices(this IServiceCollection services)
+    {
+        services.AddScoped<IAccountApiService, AccountApiService>();
+        services.AddScoped<IUserApiService, UserApiService>();
+        services.AddScoped<IWalletApiService, WalletApiService>();
+
+        return services;
+    }  
+    public static IServiceCollection AddHostedServices(this IServiceCollection services)
+    {
+        services.AddHostedService<KeyRotationService>();
+
+        return services;
+    }
+}
--- BankingAPI/Program.cs
+++ BankingAPI/Program.cs
@@ -1,83 +1,19 @@
-using System.IdentityModel.Tokens.Jwt;
+using BankingAPI.Extensions;
 using BankingAPi.Infrastructure;
 using BankingAPi.Infrastructure.Extensions;
-using BankingAPi.Infrastructure.Options;
-using BankingAPI.Interfaces.Api;
-using BankingAPI.Interfaces.Domain;
 using BankingAPI.Middlewares;
-using BankingAPI.Services.Api;
-using BankingAPI.Services.Background;
-using BankingAPI.Services.Domain;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Options;
-using Microsoft.IdentityModel.Tokens;
-using IdentityOptions = BankingAPI.Options.IdentityOptions;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
-builder.Services.Configure<DatabaseOptions>(builder.Configuration.GetSection(nameof(DatabaseOptions)));
-builder.Services.Configure<IdentityOptions>(builder.Configuration.GetSection(nameof(BankingAPI.Options.IdentityOptions)));
+builder.Services.AddOptions(builder.Configuration);
+builder.Services.AddSqlDatabase();
+builder.Services.AddJwtAuthentication(builder.Configuration);
+builder.Services.AddServices().AddApiServices().AddHostedServices();
 
-builder.Services.AddDbContext<ApplicationDbContext>
-(
-    (serviceProvider, opt) =>
-    {
-        var dbOptions = serviceProvider.GetRequiredService<IOptions<DatabaseOptions>>().Value;
-        opt.UseSqlServer(dbOptions.ConnectionString);
-        opt.EnableDetailedErrors();
-    }
-);
+builder.Services.AddControllers();
 
-builder.Services.AddControllers();//addJsonOptions
-builder.Services.AddHostedService<KeyRotationService>();
-
-builder.Services.AddAuthentication(opt =>
-{
-    opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-}).AddJwtBearer(opt =>
-{
-    JwtSecurityTokenHandler.DefaultMapInboundClaims = false;
-    JwtSecurityTokenHandler.DefaultOutboundClaimTypeMap.Clear();
-    JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
-    var identityOptions = new IdentityOptions();
-    builder.Configuration.GetSection(nameof(IdentityOptions)).Bind(identityOptions);
-    opt.TokenValidationParameters = new TokenValidationParameters()
-    {
-        ValidateIssuer = true,
-        ValidIssuer = identityOptions.Issuer,
-        ValidAudience = identityOptions.Audience,
-        ValidateAudience = true,
-        ValidateLifetime = true,
-        ValidateIssuerSigningKey = true,
-
-        IssuerSigningKeyResolver = (token, securityToken, kid, parameters) =>
-        {
-            var httpClient = new HttpClient();
-
-            var options = builder.Configuration.GetSection(nameof(IdentityOptions)).Get<IdentityOptions>();
-            
-            var jwks = httpClient.GetStringAsync($"{options.Issuer}/.well-known/jwks.json").Result;
-            // Parse the fetched JWKS into a JsonWebKeySet object
-            var keys = new JsonWebKeySet(jwks);
-
-            return keys.Keys;
-        }
-    };
-});
-
-builder.Services.AddScoped<IAccountApiService, AccountApiService>();
-builder.Services.AddScoped<IAccountService, AccountService>();
-builder.Services.AddScoped<IUserService, UserService>();
-builder.Services.AddScoped<IUserApiService, UserApiService>();
-builder.Services.AddScoped<IWalletService, WalletService>();
-builder.Services.AddScoped<IWalletApiService, WalletApiService>();
 builder.Services.AddHttpContextAccessor();
 var app = builder.Build();
 
